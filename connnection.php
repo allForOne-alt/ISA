ee0189820f35524e15a6e8f7c32d1cd441744b24
allForOne-alt <weeesdom1@gmail.com>
2026-01-17 12:20:04 +0545

    minor changes

--- connnection.php
+++ connnection.php
@@ -2,7 +2,7 @@
 $serverName = "localhost";
 $userName = "root";
 $password = "";
-$api_key = "8e67b2065f4c070146565abbdcbc8616";
+$api_key = "API_KEY";
 $conn = mysqli_connect($serverName, $userName, $password);
 
 if($conn) {
@@ -109,4 +109,5 @@ header('Content-Type: application/json');
 echo $json_data;
 
 mysqli_close($conn);
+
 ?>
